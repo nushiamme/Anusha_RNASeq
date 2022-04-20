fd1bce2a60ce3bc32af1a827da593251f979eee0
Anusha Shankar <nushiamme@gmail.com>
2022-04-20 19:35:20 -0400

    Made some prelim heat maps, made a bunch of enriched volcano plots. Filtered dataset for each pairwise comparison by adj p-cal, logFC, and baseMean. Now going to try to plot the filtered datasets for heatmaps

--- deseq_test.R
+++ deseq_test.R
@@ -4,11 +4,15 @@
 
 ## Initially followed this tutorial:
 ## https://www.youtube.com/watch?v=OzNzO8qwwp0
+## And then for visualizations
+## https://github.com/hbctraining/DGE_workshop/blob/master/lessons/06_DGE_visualizing_results.md
+## For GSEA: https://www.youtube.com/watch?v=KY6SS4vRchY
 
 # if (!requireNamespace('BiocManager', quietly = TRUE))
 #   install.packages('BiocManager')
 # BiocManager::install('DESeq2')
 # BiocManager::install('EnhancedVolcano') 
+# BiocManager::install('apeglm') 
 # Enhanced Volcano plot help: 
 # https://bioconductor.org/packages/devel/bioc/vignettes/EnhancedVolcano/inst/doc/EnhancedVolcano.html
 
@@ -20,6 +24,11 @@ library(dplyr)
 library(here)
 library(viridis)
 library(gridExtra)
+library(apeglm)
+library(RColorBrewer)
+library(pheatmap)
+library(clusterProfiler)
+
 
 ## If you opened the .Rproj file from the OneDrive folder, reading in these files will work- 
 ## the relative paths should be the same
@@ -32,6 +41,8 @@ foldchange <- read.csv(here("..//DESeq_Data_Mar2022_all tissues//all tissues//fi
 my_theme <- theme_classic(base_size = 15) + 
   theme(panel.border = element_rect(colour = "black", fill=NA)) + theme(legend.key.height = unit(2, "line"))
 
+## Viridis colors
+my_gradient <- c("#823de9", "#7855ce", "#6e6eb2", "#648697", "#599e7c", "#4fb760", "#45cf45")
 
 ## Checking on the star alignment
 head(star)
@@ -108,14 +119,51 @@ dds$Metabolic_State <- relevel(dds$Metabolic_State, ref="N")
 ## Run DESeq
 dds <- DESeq(dds)
 
+## Get normalized data, save it
+normalized_counts <- counts(dds, normalized=TRUE)
+norm_counts_df <- as.data.frame(normalized_counts)
+write.csv(norm_counts_df, file = here("..//DESeq_Data_Mar2022_all tissues//all tissues//final//RNASeq_NormCounts.csv"))
+
+## For GSEA, make a file that has all the groups in a row in the order they are in in the columns of the the normalized 
+## counts dataset. Then in Excel, add a top row that has e.g. <119 2 1> where 
+## 119 is the number of samples; 2 is the number of groups and 1 is a constant for all files.
+## Add a second row that has <# Trt1 Trt2 Cntrl> with spaces in between where each substring is the name of the treatment group 
+## Encompassing all the cell values below
+meta2$Tissue_State <- paste0(meta2$Tissue, "_", meta2$Metabolic_State)
+
+phenotype_labs <- data.frame()
+
+phenotype_labs_state <- rbind(phenotype_labs, meta2$Metabolic_State)
+write.csv(phenotype_labs_state, file = here("..//DESeq_Data_Mar2022_all tissues//all tissues//final//Pheno_labs_state.csv"))
+
+phenotype_labs_tissue <- rbind(phenotype_labs, meta2$Tissue)
+write.csv(phenotype_labs_state, file = here("..//DESeq_Data_Mar2022_all tissues//all tissues//final//Pheno_labs_tissue.csv"))
+
+phenotype_labs_state <- rbind(phenotype_labs, meta2$Tissue_State)
+write.csv(phenotype_labs_state, file = here("..//DESeq_Data_Mar2022_all tissues//all tissues//final//Pheno_labs_tissue_state.csv"))
+
+
 ## If you have technical replicates, collapse them now. We don't.. we only have biological replicates. 
 # Do not collapse those.
 
+
+
+## Take a look at the data a bit
+## Total number of raw counts per sample
+colSums(counts(dds))
+
+## Total number of normalized counts per sample
+colSums(counts(dds, normalized=T))
+
+## Plot dispersion estimates
+plotDispEsts(dds)
+
 ## Save results
+res_unshrunken <- results(dds)
 res <- results(dds)
 
 ## Take a quick look at the results
-res
+res_unshrunken
 
 ## Summary of results
 summary(res)
@@ -128,29 +176,222 @@ summary(res_NT)
 res_TD <- results(dds, contrast=c("Metabolic_State", "D", "T"))
 summary(res_TD)
 
-### Adjusted p values of 0.01
-res_ND_0.01 <- results(dds, contrast=c("Metabolic_State", "D", "N"), alpha=0.01)
-summary(res_ND_0.01)
-res_NT_0.01 <- results(dds, contrast=c("Metabolic_State", "T", "N"), alpha=0.01)
-summary(res_NT_0.01)
-res_TD_0.01 <- results(dds, contrast=c("Metabolic_State", "D", "T"), alpha=0.01)
-summary(res_TD_0.01)
+# ### Adjusted p values of 0.01
+# res_ND_0.01 <- results(dds, contrast=c("Metabolic_State", "D", "N"), alpha=0.01)
+# summary(res_ND_0.01)
+# res_NT_0.01 <- results(dds, contrast=c("Metabolic_State", "T", "N"), alpha=0.01)
+# summary(res_NT_0.01)
+# res_TD_0.01 <- results(dds, contrast=c("Metabolic_State", "D", "T"), alpha=0.01)
+# summary(res_TD_0.01)
 
-## Shrinking??
-#res <- lfcShrink(dds, coef = 2, res = res)
+## Shrinking
+res <- lfcShrink(dds, coef = 3, res = res)
 #res
 
-## MA plot
-plotMA(res_ND)
+## Fold change results
+mcols(res, use.names=T)
+res %>% data.frame() %>% View()
+## Summarize results
+summary(res)
+
+### Set thresholds
+padj.cutoff <- 0.05
+lfc.cutoff <- 0.58 ## equal to fold change of 1.5
+
+## We can easily subset the results table to only include 
+## those that are significant using the filter() function, but first we will convert the results table into a tibble:
+res_tb <- res %>%
+  data.frame() %>%
+  rownames_to_column(var="gene") %>% 
+  as_tibble()
+
+## Just subset significantly different genes
+sig <- res_tb %>%
+  filter(padj < padj.cutoff & abs(log2FoldChange) > lfc.cutoff)
+
+# Create tibbles including row names of normalized counts
+normalized_counts <- normalized_counts %>% 
+  data.frame() %>%
+  rownames_to_column(var="gene") %>% 
+  as_tibble()
+
+## Order results by padj values
+top30_sig_genes <- res_tb %>% 
+  arrange(padj) %>% 	#Arrange rows by padj values
+  pull(gene) %>% 		#Extract character vector of ordered genes
+  head(n=30) 		#Extract the first 30 genes
+
+# ## Order results by padj values, just ND and just upreg genes
+# top30_sig_genes_upreg <- res_ND_tb %>% 
+#   arrange(padj) %>% 	#Arrange rows by padj values
+#   filter(log2FoldChange>1) %>% ### NOT WORKING YET
+#   pull(gene) %>% 		#Extract character vector of ordered genes
+#   head(n=30) 		#Extract the first 30 genes
+
+## For enrichr analysis, just filtering out genes that have basemean >10, logFC >1 or < -1,
+# And adjusted p-val of < 0.05
+
+downreg_ND <-  res_ND %>%
+  data.frame() %>%
+  rownames_to_column(var="gene") %>% 
+  as_tibble() %>%
+  filter(log2FoldChange>1 & padj < 0.05 & baseMean > 10)
+
+upreg_ND <-  res_ND %>%
+  data.frame() %>%
+  rownames_to_column(var="gene") %>% 
+  as_tibble() %>%
+  filter(log2FoldChange< -1 & padj < 0.05 & baseMean > 10)
+
+## For Transition vs. Normo
+downreg_NT <-  res_NT %>%
+  data.frame() %>%
+  rownames_to_column(var="gene") %>% 
+  as_tibble() %>%
+  filter(log2FoldChange>1 & padj < 0.05 & baseMean > 10)
+
+upreg_NT <-  res_NT %>%
+  data.frame() %>%
+  rownames_to_column(var="gene") %>% 
+  as_tibble() %>%
+  filter(log2FoldChange< -1 & padj < 0.05 & baseMean > 10)
+
+## For deep torpor vs. transition
+downreg_TD <-  res_TD %>%
+  data.frame() %>%
+  rownames_to_column(var="gene") %>% 
+  as_tibble() %>%
+  filter(log2FoldChange>1 & padj < 0.05 & baseMean > 10)
+
+upreg_TD <-  res_TD %>%
+  data.frame() %>%
+  rownames_to_column(var="gene") %>% 
+  as_tibble() %>%
+  filter(log2FoldChange< -1 & padj < 0.05 & baseMean > 10)
+
+
+## normalized counts for top 20 significant genes
+top30_sig_norm <- normalized_counts %>%
+  filter(gene %in% top30_sig_genes)
+
+# Gathering the columns to have normalized counts to a single column
+gathered_top30_sig <- top30_sig_norm %>%
+  gather(colnames(top30_sig_norm)[2:120], key = "Sample", value = "normalized_counts")
+
+## check the column header in the "gathered" data frame
+View(gathered_top30_sig)
+
+gathered_top30_sig <- inner_join(meta2, gathered_top30_sig, by="Sample")
+
+## Meh plot using ggplot2
+ggplot(gathered_top30_sig) + #facet_grid(.~Metabolic_State) +
+  geom_point(aes(x = gene, y = normalized_counts, color = Tissue)) +
+  scale_y_log10() + 
+  xlab("Genes") +
+  ylab("log10 Normalized Counts") +
+  ggtitle("Top 30 Significant DE Genes") +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  theme(plot.title = element_text(hjust = 0.5)) + scale_color_viridis_d()
+
+
+
+### Extract normalized expression for significant genes from the samples 
+# (in e.g. it was 4:9), and set the gene column (1) to row names
+norm_sig <- normalized_counts[,c(1, 2:120)] %>% 
+  filter(gene %in% sig$gene) %>% 
+  data.frame() %>%
+  column_to_rownames(var = "gene") 
+
+### Annotate our heatmap (optional)
+annotation <- meta2 %>% 
+  select(Sample, Tissue) %>% 
+  data.frame(row.names = "Sample")
+
+### Set a color palette
+heat_colors <- brewer.pal(9, "YlOrRd")
+
+### Run pheatmap without col clusters
+pheatmap(norm_sig, 
+         color = heat_colors, 
+         cluster_rows = T, 
+         show_rownames = F,
+         annotation = annotation, 
+         border_color = NA, 
+         fontsize = 10, 
+         scale = "row", 
+         fontsize_row = 10, 
+         height = 20,
+         cluster_cols = FALSE)
+
+### Run pheatmap with col clusters
+pheatmap(norm_sig, 
+         color = heat_colors, 
+         cluster_rows = T, 
+         show_rownames = F,
+         annotation = annotation, 
+         border_color = NA, 
+         fontsize = 10, 
+         scale = "row", 
+         fontsize_row = 10, 
+         height = 20,
+         cluster_cols = T)
+
+annotation_col <- data.frame(
+  Tissue = factor(meta2$Tissue), 
+  Metab = factor(meta$Metabolic_State))
+
+#rownames(annotation_col) = paste("Test", 1:10, sep = "")
+
+# annotation_row <- data.frame(
+#   GeneClass = factor(rep(c("Path1", "Path2", "Path3"), c(10, 4, 6)))
+# )
+# rownames(annotation_row) = paste("Gene", 1:20, sep = "")
+
+ann_colors = list(
+  Tissue = c(Liver = "#1B9E77", Gut1 = "#D95F02", Gut2 = "#823de9", Gut3 = "#7855ce",
+             Heart = "#6e6eb2", Lungs = "#648697", Pect = "#599e7c"),
+  Metab = c(N = "#7570B3", T = "#E7298A", D = "#66A61E")
+)
+
+#c("#823de9", "#7855ce", "#6e6eb2", "#648697", "#599e7c", "#4fb760", "#45cf45")
+
+pheatmap(norm_sig, annotation_col = annotation_col,#, #annotation_row = annotation_row, 
+         annotation_colors = ann_colors,
+         #cluster_rows = T, 
+         show_rownames = F,
+         annotation = annotation, 
+         border_color = NA, 
+         fontsize = 10, 
+         scale = "row", 
+         fontsize_row = 10, 
+         height = 20,
+         cluster_cols = FALSE)
+
+
+pheatmap(norm_sig, annotation_col = annotation_col,
+         annotation_colors = ann_colors,
+         #cluster_rows = T, 
+         color = heat_colors, 
+         show_rownames = F,
+         annotation = annotation, 
+         border_color = NA, 
+         fontsize = 10, 
+         scale = "row", 
+         fontsize_row = 10, 
+         height = 20)
+
+
 
 ## Volcano plot normothermy vs deep torpor
+#### MAKE SURE THIS IS ADJUSTED P-VAL
 ND_volcano <- EnhancedVolcano(res_ND,
                 lab = rownames(res_ND),
                 x = 'log2FoldChange',
                 y = 'pvalue',
                 title = 'Deep torpor vs. Normothermy',
-                subtitle = "Enhanced volcano, p cutoff = 10e-10",
-                pCutoff = 10e-10)
+                subtitle = "Enhanced volcano, p cutoff = 0.05",
+                pCutoff = 0.05)
 
 
 ## Volcano plot normothermy vs deep torpor
@@ -159,8 +400,8 @@ NT_volcano <- EnhancedVolcano(res_NT,
                 x = 'log2FoldChange',
                 y = 'pvalue',
                 title = 'Transition vs. Normothermy',
-                subtitle = "Enhanced volcano, p cutoff = 10e-10",
-                pCutoff = 10e-10)
+                subtitle = "Enhanced volcano, p cutoff = 0.05",
+                pCutoff = 0.05)
 
 ## Volcano plot normothermy vs deep torpor
 TD_volcano <- EnhancedVolcano(res_TD,
@@ -168,13 +409,58 @@ TD_volcano <- EnhancedVolcano(res_TD,
                 x = 'log2FoldChange',
                 y = 'pvalue',
                 title = 'Deep Torpor vs. Transition',
-                subtitle = "Enhanced volcano, p cutoff = 10e-10",
-                pCutoff = 10e-10)
+                subtitle = "Enhanced volcano, p cutoff = 0.05",
+                pCutoff = 0.05)
 
 grid.arrange(ND_volcano, NT_volcano, TD_volcano, ncol=3)
 
+## Use adjusted p-vals to make plots instead of nominal p-values
+ND_adj_volcano <- EnhancedVolcano(res_ND, lab = rownames(res_ND),
+                x = 'log2FoldChange',
+                y = 'padj',
+                #xlab = bquote(~Log[2]~ "fold change"),
+                ylab = bquote(-Log[10]~adjusted~italic(P)),
+                pCutoff = 0.05,
+                FCcutoff = 1.0,
+                #transcriptLabSize = 3.0,
+                colAlpha = 1,
+                title = 'Deep Torpor vs. Normothermy',
+                subtitle = "Enhanced volcano, adj p cutoff = 0.05")
+                #legend=c("NS","Log2 FC","Adjusted p-value",
+                 #        "Adjusted p-value & Log2 FC"),
+                #legendPosition = "bottom",
+                #legendLabSize = 10,
+                #legendIconSize = 3.0)
 
+NT_adj_volcano <- EnhancedVolcano(res_NT, lab = rownames(res_NT),
+                              x = 'log2FoldChange',
+                              y = 'padj',
+                              #xlab = bquote(~Log[2]~ "fold change"),
+                              ylab = bquote(-Log[10]~adjusted~italic(P)),
+                              pCutoff = 0.05,
+                              FCcutoff = 1.0,
+                              #transcriptLabSize = 3.0,
+                              colAlpha = 1,
+                              title = 'Transition vs. Normothermy',
+                              subtitle = "Enhanced volcano, adj p cutoff = 0.05")
 
+TD_adj_volcano <- EnhancedVolcano(res_TD, lab = rownames(res_TD),
+                              x = 'log2FoldChange',
+                              y = 'padj',
+                              #xlab = bquote(~Log[2]~ "fold change"),
+                              ylab = bquote(-Log[10]~adjusted~italic(P)),
+                              pCutoff = 0.05,
+                              FCcutoff = 1.0,
+                              #transcriptLabSize = 3.0,
+                              colAlpha = 1,
+                              title = 'Deep Torpor vs. Transition',
+                              subtitle = "Enhanced volcano, adj p cutoff = 0.05")
+
+grid.arrange(ND_adj_volcano, NT_adj_volcano, TD_adj_volcano, ncol=3)
+
+ND_adj_volcano
+NT_adj_volcano
+TD_adj_volcano
 
 ## geom_density
 datlong %>%
@@ -202,6 +488,7 @@ datlong %>%
   scale_fill_gradient(low = 'white', high = 'red') + facet_grid(.~Metabolic_State, scales = "free")
 
 
+## Examples
 genes.of.interest <- c('NR1D1', 'SGK1', 'RSRP1', 'DUSP1', 'NME5', 'TMEM39B', 'BHLHE40', 'LOC115598688')
 foldlong %>%
   filter(Measure %in% c('D_vs_N_log2FoldChange', 'T_vs_N_log2FoldChange', 'T_vs_D_log2FoldChange')) %>%
@@ -209,5 +496,52 @@ foldlong %>%
   geom_tile() + my_theme + facet_grid(.~Tissue) +
   scale_fill_gradient(low = 'yellow', high = 'red')
 
+foldlong %>%
+  filter(Measure %in% c('D_vs_N_log2FoldChange', 'T_vs_N_log2FoldChange', 'T_vs_D_log2FoldChange')) %>%
+  ggplot(., aes(x = Measure, y = gene, fill = value)) +
+  geom_tile() + my_theme + facet_grid(.~Tissue) +
+  scale_fill_gradient(low = 'yellow', high = 'red')
+
+
+## For enrichr analysis, just filtering out genes that have basemean >10, logFC >1 or < -1,
+# And adjusted p-val of < 0.05
+
+downreg_ND <-  res_ND %>%
+  data.frame() %>%
+  rownames_to_column(var="gene") %>% 
+  as_tibble() %>%
+  filter(log2FoldChange>1 & padj < 0.05 & baseMean > 10)
+
+upreg_ND <-  res_ND %>%
+  data.frame() %>%
+  rownames_to_column(var="gene") %>% 
+  as_tibble() %>%
+  filter(log2FoldChange< -1 & padj < 0.05 & baseMean > 10)
+
+## For Transition vs. Normo
+downreg_NT <-  res_NT %>%
+  data.frame() %>%
+  rownames_to_column(var="gene") %>% 
+  as_tibble() %>%
+  filter(log2FoldChange>1 & padj < 0.05 & baseMean > 10)
+
+upreg_NT <-  res_NT %>%
+  data.frame() %>%
+  rownames_to_column(var="gene") %>% 
+  as_tibble() %>%
+  filter(log2FoldChange< -1 & padj < 0.05 & baseMean > 10)
+
+## For deep torpor vs. transition
+downreg_TD <-  res_TD %>%
+  data.frame() %>%
+  rownames_to_column(var="gene") %>% 
+  as_tibble() %>%
+  filter(log2FoldChange>1 & padj < 0.05 & baseMean > 10)
+
+upreg_TD <-  res_TD %>%
+  data.frame() %>%
+  rownames_to_column(var="gene") %>% 
+  as_tibble() %>%
+  filter(log2FoldChange< -1 & padj < 0.05 & baseMean > 10)
 
 
---
+++ functional_analysis.R
@@ -0,0 +1,11 @@
+## Functional analysis
+## Script started April 7, 2022
+## Anusha Shankar, github/nushiamme
+
+library(org.Hs.eg.db)
+library(DOSE)
+library(pathview)
+library(clusterProfiler)
+library(AnnotationHub)
+library(ensembldb)
+library(tidyverse)
